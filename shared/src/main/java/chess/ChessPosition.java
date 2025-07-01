241067bfb7f7a9749a584c301a153f2a154c2ba6
kkeetch13 <kkeetch76@gmail.com>
2025-06-30 23:28:31 -0600

    replaced 'that' variable with pattern variable

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -37,8 +37,7 @@ public class ChessPosition {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof ChessPosition)) return false;
-        ChessPosition that = (ChessPosition) o;
+        if (!(o instanceof ChessPosition that)) return false;
         return row == that.row && col == that.col;
     }
 
